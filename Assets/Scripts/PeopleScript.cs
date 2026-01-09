1a733add55aeca0518e591dabb3768efad1188b1
hewabadd <hewabadd@OA-S145-04>
2026-01-09 17:51:51 -0500

    added people prefabs that move around randomly

---
+++ Assets/Scripts/PeopleScript.cs
@@ -0,0 +1,52 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PeopleScript : MonoBehaviour
+{
+    Rigidbody rb;
+    public float waitDuration = 4, moveDuration = 2, minDist, maxDist;
+    bool moving = false, pickedDir = false;
+    Vector3 force;
+    float t;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        t += Time.deltaTime;
+        
+
+        if (!moving)
+        {
+            if (t > waitDuration)
+            {
+                if (!pickedDir)
+                {
+                    force = PickRandomDirection();
+                }
+                moving = true;
+                t = 0;
+            }
+        } else
+        {
+            if (t > moveDuration)
+            {
+                moving = false;
+                t = 0;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (moving) 
+        { 
+            rb.AddForce(force, ForceMode.Impulse);
+        }
+    }
+
+    private Vector3 PickRandomDirection()
+    {
+        return new Vector3(Random.Range(minDist, maxDist), 0, Random.Range(minDist, maxDist));
+    }
+}
