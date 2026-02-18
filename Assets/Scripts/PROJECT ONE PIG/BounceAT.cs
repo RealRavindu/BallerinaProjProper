0e801dd4fecc0221b34d7689086de7e01bc4430b
ravin <hewabadd@sheridancollege.ca>
2026-02-17 19:50:40 -0500

    added bouncing only while in pool.

--- Assets/Scripts/PROJECT ONE PIG/BounceAT.cs
+++ Assets/Scripts/PROJECT ONE PIG/BounceAT.cs
@@ -2,29 +2,55 @@ using NodeCanvas.Framework;
 using ParadoxNotion.Design;
 using UnityEngine;
 
-namespace NodeCanvas.Tasks.Actions {
+namespace NodeCanvas.Tasks.Actions
+{
 
-	public class BounceAT : ActionTask {
-		public float frequency, amplitude;
-		private Vector3 originalPosition;
-		protected override string OnInit() {
-			originalPosition = agent.transform.position;
-			return null;
-		}
+    public class BounceAT : ActionTask
+    {
+        public float frequency, amplitude;
+        private Vector3 originalPosition;
+        public bool bounceInWaterOnly = false;
+        private bool buoyancy = false;
+        public LayerMask pondLayer;
+        protected override string OnInit()
+        {
+            originalPosition = agent.transform.position;
+            return null;
+        }
 
-		protected override void OnExecute() {
-		}
+        protected override void OnExecute()
+        {
+        }
 
-		protected override void OnUpdate() {
-				agent.transform.position = new Vector3(agent.transform.position.x, Mathf.Sin(Time.time * frequency) * amplitude + agent.transform.position.y, agent.transform.position.z);
-		}
+        protected override void OnUpdate()
+        {
+            buoyancy = (Physics.Raycast(agent.transform.position, -agent.transform.up, 10, pondLayer));
 
-		protected override void OnStop() {
-			agent.transform.position = originalPosition;
-		}
+            if (bounceInWaterOnly)
+            {
+                if (buoyancy) Bounce();
 
-		protected override void OnPause() {
-			
-		}
-	}
+            }
+            else
+            {
+                Bounce();
+            }
+
+        }
+
+        protected override void OnStop()
+        {
+            agent.transform.position = originalPosition;
+        }
+
+        protected override void OnPause()
+        {
+
+        }
+
+        private void Bounce()
+        {
+            agent.transform.position = new Vector3(agent.transform.position.x, Mathf.Sin(Time.time * frequency) * amplitude + agent.transform.position.y, agent.transform.position.z);
+        }
+    }
 }
